58eb8dba1a23a60c53e79464667f8a301078aa83
Robert Edmonds <bobby.edmonds89@gmail.com>
2022-03-20 16:35:26 -0600

    commit tree 422f5827889d447060d0e0db991a72e295e4a83d

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,42 @@
-import React, { useState } from "react";
+import React, {useEffect, useState } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [isQuestion, setIsQuestion] = useState([])
+
+  useEffect(()=>{
+    fetch("http://localhost:4000/questions")
+    .then(resp => resp.json())
+    .then(item => setIsQuestion(item))
+  }, [])
+
+  function handleAddItem(newItem){
+    setIsQuestion([...isQuestion, newItem])
+  }
+  
+  function handleDeleteQuestion(deletedItem){
+    const updateItems = isQuestion.filter(item => item.id !== deletedItem.id)
+    setIsQuestion(updateItems)
+  }
+
+  function handleAnswerChange(newAnswer){
+    const updateItems = isQuestion.map(item=>{
+      if(item.id === newAnswer.id){
+        return newAnswer
+      }else{
+        return item
+      }
+    })
+    setIsQuestion(updateItems)
+  }
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm onAddItem={handleAddItem}/> : <QuestionList isQuestion={isQuestion} onDeleteItem={handleDeleteQuestion} onAnswerChange={handleAnswerChange}/>}
     </main>
   );
 }
--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-function QuestionForm(props) {
+function QuestionForm({onAddItem}) {
   const [formData, setFormData] = useState({
     prompt: "",
     answer1: "",
@@ -19,7 +19,20 @@ function QuestionForm(props) {
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(formData);
+    const formPost = {
+      prompt: formData.prompt,
+      answers: [formData.answer1, formData.answer2, formData.answer3, formData.answer4],
+      correctIndex: formData.correctIndex 
+    }
+    fetch("http://localhost:4000/questions",{
+      method: "POST",
+      headers:{
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(formPost)
+    })
+    .then(resp => resp.json())
+    .then(newItem => onAddItem(newItem))
   }
 
   return (
